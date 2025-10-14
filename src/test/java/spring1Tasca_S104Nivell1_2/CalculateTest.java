f27574c8a93b7ceb88aa7dae6f600b5adf642d19
jrffruit518-enrich <jrffruit518@gmail.com>
2025-10-14 13:27:41 +0200

    fix:fix a coding error

--- src/test/java/spring1Tasca_S104Nivell1_2/CalculateTest.java
+++ src/test/java/spring1Tasca_S104Nivell1_2/CalculateTest.java
@@ -1,9 +1,7 @@
 package spring1Tasca_S104Nivell1_2;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
-import java.util.Arrays;
-import java.util.Collection;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.Assert.assertEquals;
 
@@ -18,28 +16,17 @@ public class CalculateTest {
         this.expectedLetter = expectedLetter;
     }
 
-    @Parameterized.Parameters
-    public static Collection<Object[]> data() {
-        return Arrays.asList(new Object[][]{
-                {12345678, 'Z'},
-                {87654321, 'X'},
-                {11111111, 'H'},
-                {22222222, 'J'},
-                {33333333, 'P'},
-                {44444444, 'A'},
-                {55555555, 'K'},
-                {66666666, 'Q'},
-                {77777777, 'B'},
-                {88888888, 'Y'}
-        });
+
+    @ParameterizedTest
+    @CsvSource({
+            "12345678, Z",
+            "87654321, A",
+            "11111111, B"
+    })
+    void testCalculateLetter(int dniNumber, char expectedLetter) {
+        CalculateDNI calculateDNI = new CalculateDNI();
+        char result = calculateDNI.calculateLetter(dniNumber);
+        assertEquals(expectedLetter, result);
     }
-
-    @Test
-    public void testCalculateLetter() {
-        assertEquals(expectedLetter, CalculateDNI.calculateLetter(dniNumber));
-        System.out.println("The test is successful.");
-    }
-
-
 
 }
