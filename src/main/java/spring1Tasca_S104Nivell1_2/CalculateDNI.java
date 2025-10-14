fc02eec41e005f0cf9375af44050ee9b30fdf170
jrffruit518-enrich <jrffruit518@gmail.com>
2025-10-14 12:18:53 +0200

    feat : add spring1Tasca_S104Nivell1_2

---
+++ src/main/java/spring1Tasca_S104Nivell1_2/CalculateDNI.java
@@ -0,0 +1,22 @@
+package spring1Tasca_S104Nivell1_2;
+
+public class CalculateDNI {
+
+    private static final char[] LETTERS = {
+            'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D',
+            'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L',
+            'C', 'K', 'E'
+    };
+
+
+    public static char calculateLetter(int dniNumber) {
+        if (dniNumber < 10000000 || dniNumber > 99999999) {
+            throw new IllegalArgumentException("DNI number must be between 10000000 and 99999999.");
+        }
+        int index = dniNumber % 23;
+        return LETTERS[index];
+    }
+}
+
+
+
---
+++ src/test/java/spring1Tasca_S104Nivell1_2/CalculateTest.java
@@ -0,0 +1,47 @@
+package spring1Tasca_S104Nivell1_2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class CalculateTest {
+
+    private final int dniNumber;
+    private final char expectedLetter;
+
+    public CalculateTest(int dniNumber, char expectedLetter) {
+        this.dniNumber = dniNumber;
+        this.expectedLetter = expectedLetter;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {12345678, 'Z'},
+                {87654321, 'X'},
+                {11111111, 'H'},
+                {22222222, 'J'},
+                {33333333, 'P'},
+                {44444444, 'A'},
+                {55555555, 'K'},
+                {66666666, 'Q'},
+                {77777777, 'B'},
+                {88888888, 'Y'}
+        });
+    }
+
+    @Test
+    public void testCalculateLetter() {
+        assertEquals(expectedLetter, CalculateDNI.calculateLetter(dniNumber));
+        System.out.println("The test is successful.");
+    }
+
+
+
+}
