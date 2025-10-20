beca4c8377f23ec92770091d0ad9363acb9d3697
jrffruit518-enrich <jrffruit518@gmail.com>
2025-10-20 12:47:57 +0200

    fix: - correct the code of LibraryTest.java

--- src/test/java/sprint1Tasca_S104Nivell1_1/LibraryTest.java
+++ src/test/java/sprint1Tasca_S104Nivell1_1/LibraryTest.java
@@ -2,9 +2,10 @@ package sprint1Tasca_S104Nivell1_1;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import java.util.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class LibraryTest {
@@ -17,121 +18,103 @@ public class LibraryTest {
 
     @Test
     public void testBookListIsNotNullAfterCreation() {
-        if (library.getBookManagement().getBooks() == null) {
-            throw new RuntimeException("The books is null. Failed.");
-        } else {
-            System.out.println(("The books is not null. Test is successful."));
-        }
+        assertNotNull(library.getBooks(), "The book list should not be null after creating a new Library object.");
 
     }
 
     @Test
     public void testAddMultipleBooksSize() {
-        try {
-            library.getBookManagement().addBook("book1");
-            library.getBookManagement().addBook("book2");
-            if (library.getBookManagement().getBooks().size() == 2) {
-                System.out.println("The test is successful.");
-            } else {
-                System.out.println("The test is failed.");
-            }
-        } catch (ExceptionBookExisted e) {
-            System.out.println(e.getMessage());
-        }
+
+        library.addBook(new Book("A Tale of Two Cities"));
+        library.addBook(new Book("Cien años de soledad"));
+        library.addBook(new Book("Hamlet"));
+        library.addBook(new Book("zoo"));
+        library.addBook(new Book("1984"));
+
     }
+
 
     @Test
     public void testListIsAlphabeticallyOrdered() {
-        try {
-            library.getBookManagement().addBook("Z");
-            library.getBookManagement().addBook("L");
-            library.getBookManagement().addBook("A");
-            ArrayList<String> listTitle = new ArrayList<>();
-            Iterator<Book> iterator = library.getBookManagement().getBooks().iterator();
-            while (iterator.hasNext()) {
-                listTitle.add(iterator.next().getName());
-            }
-            ArrayList<String> listSort = new ArrayList<>(listTitle);
-            Collections.sort(listSort);
-            assertEquals(listSort, listTitle,"The test is failed.");
-            System.out.println("The test is successful.");
-        } catch (ExceptionBookExisted e) {
-            System.out.println(e.getMessage());
+        library.addBook(new Book("A Tale of Two Cities"));
+        library.addBook(new Book("Cien años de soledad"));
+        library.addBook(new Book("Hamlet"));
+        library.addBook(new Book("zoo"));
+        library.addBook(new Book("1984"));
+        ArrayList<String> listTitle = new ArrayList<>();
+        Iterator<Book> iterator = library.getBooks().iterator();
+        while (iterator.hasNext()) {
+            listTitle.add(iterator.next().getName());
         }
+        ArrayList<String> listSort = new ArrayList<>(listTitle);
+        Collections.sort(listSort);
+        assertEquals(listSort, listTitle, "The test is failed.");
     }
 
     @Test
     public void testNoDuplicateBookTitles() {
-        try {
-            library.getBookManagement().addBook("Hamlet");
-            library.getBookManagement().addBook("Zoo");
-            library.getBookManagement().addBook("1984");
-            HashSet<String> bookTitle = new HashSet<>();
-            Iterator<Book> iterator = library.getBookManagement().getBooks().iterator();
-            while (iterator.hasNext()) {
-                bookTitle.add(iterator.next().getName());
-            }
-            assertEquals(library.getBookManagement().getBooks().size(), bookTitle.size(),"The test is failed.");
-            System.out.println("The test is successful.");
-        } catch (ExceptionBookExisted e) {
-            System.out.println(e.getMessage());
+        library.addBook(new Book("A Tale of Two Cities"));
+        library.addBook(new Book("Cien años de soledad"));
+        library.addBook(new Book("Hamlet"));
+        library.addBook(new Book("zoo"));
+        library.addBook(new Book("1984"));
+        HashSet<String> bookTitle = new HashSet<>();
+        Iterator<Book> iterator = library.getBooks().iterator();
+        while (iterator.hasNext()) {
+            bookTitle.add(iterator.next().getName());
         }
+        assertEquals(library.getBooks().size(), bookTitle.size(), "The test is failed.");
 
     }
 
     @Test
     public void testGetBookByPosition() {
-        try {
-            library.getBookManagement().addBook("A Tale of Two Cities");
-            library.getBookManagement().addBook("Brave New World");
-            library.getBookManagement().addBook("Cien años de soledad");
 
-            List<Book> books = library.getBookManagement().getBooks();
+        library.addBook(new Book("A Tale of Two Cities"));
+        library.addBook(new Book("Cien años de soledad"));
+        library.addBook(new Book("Hamlet"));
+        library.addBook(new Book("zoo"));
+        library.addBook(new Book("1984"));
 
-            assertEquals("The test is failed.", "A Tale of Two Cities", books.get(0).getName());
-            assertEquals("The test is failed.", "Brave New World", books.get(1).getName());
-            assertEquals("The test is failed.", "Cien años de soledad", books.get(2).getName());
-            System.out.println("The test is successful.");
-        } catch (ExceptionBookExisted e) {
-            System.out.println(e.getMessage());
-        }
+        List<Book> books = library.getBooks();
+
+        assertEquals("The test is failed.", "A Tale of Two Cities", books.get(0).getName());
+        assertEquals("The test is failed.", "Brave New World", books.get(1).getName());
+        assertEquals("The test is failed.", "Cien años de soledad", books.get(2).getName());
+        System.out.println("The test is successful.");
 
     }
 
     @Test
     public void testAddBookIncreasesSize() {
 
-        try {
-            library.getBookManagement().addBook("A Tale of Two Cities");
-            library.getBookManagement().addBook("Brave New World");
-            library.getBookManagement().addBook("Cien años de soledad");
-            ArrayList<Book> booksInicial = new ArrayList<>(library.getBookManagement().getBooks());
 
-            assertEquals( 3, booksInicial.size(),"The test is failed.");
-            System.out.println("The test is successful.");
-        } catch (ExceptionBookExisted e) {
-            throw new RuntimeException(e);
-        }
+        library.addBook(new Book("A Tale of Two Cities"));
+        library.addBook(new Book("Cien años de soledad"));
+        library.addBook(new Book("Hamlet"));
+        library.addBook(new Book("zoo"));
+        library.addBook(new Book("1984"));
+        ArrayList<Book> booksInicial = new ArrayList<>(library.getBooks());
+
+        assertEquals(3, booksInicial.size(), "The test is failed.");
+        System.out.println("The test is successful.");
 
     }
 
     @Test
     public void testBookRemovalReducesListSize() {
 
-        try {
-            library.getBookManagement().addBook("A Tale of Two Cities");
-            library.getBookManagement().addBook("Brave New World");
-            library.getBookManagement().addBook("Cien años de soledad");
-            library.getBookManagement().removeBook("A Tale of Two Cities");
-            List<Book> books = new ArrayList<>(library.getBookManagement().getBooks());
 
-            assertEquals( 2, books.size(),"The test is failed.");
-            System.out.println("The test is successful.");
-        } catch (ExceptionBookExisted e) {
-            throw new RuntimeException(e);
-        } catch (ExceptionBookEmpty e) {
-            throw new RuntimeException(e);
-        }
+        library.addBook(new Book("A Tale of Two Cities"));
+        library.addBook(new Book("Cien años de soledad"));
+        library.addBook(new Book("Hamlet"));
+        library.addBook(new Book("zoo"));
+        library.addBook(new Book("1984"));
+        library.removeBook("A Tale of Two Cities");
+        List<Book> books = new ArrayList<>(library.getBooks());
+
+        assertEquals(2, books.size(), "The test is failed.");
+        System.out.println("The test is successful.");
 
 
     }
@@ -139,45 +122,41 @@ public class LibraryTest {
     @Test
     public void testBooksRemainSortedAfterAddOrRemove() {
 
-            try {
-                library.getBookManagement().addBook("Brave New World");
-                library.getBookManagement().addBook("Cien años de soledad");
-                library.getBookManagement().addBook("A Tale of Two Cities");
+        library.addBook(new Book("A Tale of Two Cities"));
+        library.addBook(new Book("Cien años de soledad"));
+        library.addBook(new Book("Hamlet"));
+        library.addBook(new Book("zoo"));
+        library.addBook(new Book("1984"));
+        library.removeBook("A Tale of Two Cities");
 
-                List<Book> books = library.getBookManagement().getBooks();
+        List<Book> books = library.getBooks();
 
-                for (int i = 0; i < books.size() - 1; i++) {
-                    String current = books.get(i).getName();
-                    String next = books.get(i + 1).getName();
-                    assertTrue(current.compareTo(next) <= 0,"List should be sorted alphabetically");
-                }
-
-                library.getBookManagement().addBook("Dune");
-                books = library.getBookManagement().getBooks();
-
-                for (int i = 0; i < books.size() - 1; i++) {
-                    String current = books.get(i).getName();
-                    String next = books.get(i + 1).getName();
-                    assertTrue(current.compareTo(next) <= 0,"List should remain sorted after adding a book");
-                }
-
-                library.getBookManagement().removeBook("Cien años de soledad");
-                books = library.getBookManagement().getBooks();
-
-                for (int i = 0; i < books.size() - 1; i++) {
-                    String current = books.get(i).getName();
-                    String next = books.get(i + 1).getName();
-                    assertTrue(current.compareTo(next) <= 0,"List should remain sorted after removing a book");
-                }
-
-                System.out.println("The test is successful.");
-            } catch (ExceptionBookExisted e) {
-                System.out.println(e.getMessage());
-            } catch (ExceptionBookEmpty e) {
-                System.out.println(e.getMessage());
-            }
-
+        for (int i = 0; i < books.size() - 1; i++) {
+            String current = books.get(i).getName();
+            String next = books.get(i + 1).getName();
+            assertTrue(current.compareTo(next) <= 0, "List should be sorted alphabetically");
         }
+
+        library.addBook(new Book("Dune"));
+        books = library.getBooks();
+
+        for (int i = 0; i < books.size() - 1; i++) {
+            String current = books.get(i).getName();
+            String next = books.get(i + 1).getName();
+            assertTrue(current.compareTo(next) <= 0, "List should remain sorted after adding a book");
+        }
+
+        library.removeBook("Cien años de soledad");
+        books = library.getBooks();
+
+        for (int i = 0; i < books.size() - 1; i++) {
+            String current = books.get(i).getName();
+            String next = books.get(i + 1).getName();
+            assertTrue(current.compareTo(next) <= 0, "List should remain sorted after removing a book");
+        }
+
+
+    }
 
 
 }
