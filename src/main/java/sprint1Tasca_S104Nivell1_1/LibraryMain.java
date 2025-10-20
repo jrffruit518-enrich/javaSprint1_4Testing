ddddaf6f36345dbf6623831d7d54a3a839fbbd1f
jrffruit518-enrich <jrffruit518@gmail.com>
2025-10-20 11:33:42 +0200

    fix: delete useless java file.

--- src/main/java/sprint1Tasca_S104Nivell1_1/LibraryMain.java
+++
@@ -1,8 +0,0 @@
-package sprint1Tasca_S104Nivell1_1;
-
-public class LibraryMain {
-    public static void main(String[] args) {
-        Library library = new Library();
-        library.menu();
-    }
-}
