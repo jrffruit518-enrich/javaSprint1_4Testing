ff021c72965d0dda5279a1ab22f4258c070ff57c
jrffruit518-enrich <jrffruit518@gmail.com>
2025-10-20 21:33:16 +0200

    fix : correct the code of test

--- src/test/java/sprint1Tasca_S104Nivell1_2/CalculateTest.java
+++ src/test/java/sprint1Tasca_S104Nivell1_2/CalculateTest.java
@@ -1,5 +1,6 @@
 package sprint1Tasca_S104Nivell1_2;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -8,31 +9,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculateTest {
 
-    private final int dniNumber;
-    private final char expectedLetter;
-
-    public CalculateTest(int dniNumber, char expectedLetter) {
-        this.dniNumber = dniNumber;
-        this.expectedLetter = expectedLetter;
-    }
-
-
     @ParameterizedTest
     @CsvSource({
             "12345678,Z",
-            "87654321,X",
+            "15325896,F",
             "11111111,H",
-            "22222222,J",
+            "35264578,N",
             "33333333,P",
-            "44444444,A",
+            "45285689,T",
             "55555555,K",
             "66666666,Q",
             "77777777,B",
             "88888888,Y"
     })
     void testCalculateLetter(int dniNumber, char expectedLetter) {
-        CalculateDNI calculateDNI = new CalculateDNI();
-        char result = calculateDNI.calculateLetter(dniNumber);
+        char result = CalculateDNI.calculateLetter(dniNumber);
         assertEquals(expectedLetter, result);
     }
 
