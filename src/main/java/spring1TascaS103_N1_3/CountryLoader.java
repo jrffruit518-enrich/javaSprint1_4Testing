7bf535c4195f740ecef10a4aa44b766f2521c13c
jrffruit518-enrich <jrffruit518@gmail.com>
2025-10-22 12:30:48 +0200

    fix : update the code

--- src/main/java/spring1TascaS103_N1_3/CountryLoader.java
+++ src/main/java/spring1TascaS103_N1_3/CountryLoader.java
@@ -4,28 +4,36 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 
 public class CountryLoader {
     public static HashMap<String, String> countryLoad() throws IOException {
-
-        InputStream input = CountryLoader.class.getClassLoader().getResourceAsStream("countries.txt");
-        if (input == null) {
-            throw new IOException("countries.txt not found");
-        }
-        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
         HashMap<String, String> countryCapital = new HashMap<>();
-        String line;
-        while ((line = reader.readLine()) != null) {
-            line = line.trim();
-            if (line.isEmpty()) continue;
-            String[] parts = line.split("\\s+", 2);
-            if (parts.length < 2) {
-                continue;
+
+        try(InputStream input = CountryLoader.class.getClassLoader().getResourceAsStream("countries.txt")) {
+
+            try(BufferedReader reader = new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8))) {
+
+                if (input == null) {
+                    throw new IOException("countries.txt not found");
+                }
+
+
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    line = line.trim();
+                    if (line.isEmpty()) continue;
+                    String[] parts = line.split("\\s+", 2);
+                    if (parts.length < 2) {
+                        continue;
+                    }
+                    String part0 = parts[0];
+                    String part1 = parts[1];
+                    countryCapital.put(part0, part1);
+                }
             }
-            String part0 = parts[0];
-            String part1 = parts[1];
-            countryCapital.put(part0, part1);
         }
         return countryCapital;
     }
