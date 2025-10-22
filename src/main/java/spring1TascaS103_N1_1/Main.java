49c1ad3b5d6957af6271877f21883dd242ecfb94
jrffruit518-enrich <jrffruit518@gmail.com>
2025-10-22 12:11:58 +0200

    fix : optimize the code of main       -update pom

--- src/main/java/spring1TascaS103_N1_1/Main.java
+++ src/main/java/spring1TascaS103_N1_1/Main.java
@@ -1,50 +1,63 @@
 package spring1TascaS103_N1_1;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        List<Month> months = new ArrayList<>();
-        months.addAll(Arrays.asList(Month.JANUARY, Month.FEBRUARY, Month.MARCH, Month.APRIL, Month.MAY, Month.JUNE, Month.JULY, Month.
-                SEPTEMBER, Month.OCTOBER, Month.NOVEMBER, Month.DECEMBER));
+        List<Month> months = createInitialMonthList();
 
         // No hay August
-        for (Month month : months) {
-            System.out.println(month);
-        }
-        System.out.println();
+        showFor(months);
 
-        months.add(7, Month.AUGUST);
+        addByIndex(months,7,Month.AUGUST);
 
         // Including August
-        for (Month month : months) {
+        showFor(months);
+
+        showIterator(months);
+
+        //Add duplicated January
+        addByIndex(months,12, Month.JANUARY);
+
+        showFor(months);
+
+        //Eliminate duplicated January
+        Set<Month> monthSet = removeDuplicates(months);
+        showIterator(monthSet);
+
+    }
+    public static List<Month> createInitialMonthList() {
+        return Arrays.stream(Month.values())
+                .filter(month -> month != Month.AUGUST)
+                .collect(Collectors.toList());
+    }
+
+    public static void addByIndex(List<Month>months,int index , Month month) {
+        if (index<0 || index > months.size()) {
+            throw new IllegalArgumentException("The index is invalid.");
+        }
+        months.add(index,month);
+    }
+
+    public static  void showFor(Iterable<Month> months) {
+        for (Month month : months ) {
             System.out.println(month);
         }
         System.out.println();
+    }
 
+    public static void showIterator(Iterable<Month> months) {
         Iterator<Month> iterator = months.iterator();
 
         while (iterator.hasNext()) {
             System.out.println(iterator.next());
         }
         System.out.println();
-
-        //Add duplicated January
-        months.add(Month.JANUARY);
-        for (Month month : months) {
-            System.out.println(month);
-        }
-        System.out.println();
-
-        //Eliminate duplicated January
-        HashSet<Month> hashMonths = new LinkedHashSet<>(months);
-
-        for (Month month : hashMonths) {
-            System.out.println(month);
-        }
-        System.out.println();
-
-
-
     }
+    public static Set<Month> removeDuplicates(List<Month> months) {
+        return new LinkedHashSet<>(months);
+    }
+
+
 }
