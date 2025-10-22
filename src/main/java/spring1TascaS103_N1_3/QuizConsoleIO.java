06e6dc74d6126db8f21960e0735c61d8495f0a6c
jrffruit518-enrich <jrffruit518@gmail.com>
2025-10-22 13:39:21 +0200

    feat : add QuizEngine.java        add QuizConsoleIO.java

---
+++ src/main/java/spring1TascaS103_N1_3/QuizConsoleIO.java
@@ -0,0 +1,35 @@
+package spring1TascaS103_N1_3;
+
+public class QuizConsoleIO {
+
+    public String readName() {
+    System.out.print("Please enter your name: ");
+    return EntryQuiz.readString();
+}
+
+    public String readAnswer() {
+        System.out.print("What is the capital? ");
+        return EntryQuiz.readString();
+    }
+
+    public void displayQuestion(int round, String country) {
+        System.out.printf("Round %d: What is the capital of %s? ", round, country);
+    }
+
+    public void displayCorrect() {
+        System.out.println("Correct!");
+    }
+
+    public void displayWrong(String capital) {
+        System.out.println("Wrong! The correct answer is: " + capital);
+    }
+
+    public void displayScore(String name, int score, int totalRounds) {
+        System.out.printf("Game over! %s, your final score is: %d/%d%n", name, score, totalRounds);
+    }
+
+    public void displayError(String message) {
+        System.out.println(message);
+    }
+
+}
