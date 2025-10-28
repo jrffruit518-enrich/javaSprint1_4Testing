d99bfb75206181aeeeb5f75c7b1698790c2cdb13
jrffruit518-enrich <jrffruit518@gmail.com>
2025-10-28 17:36:17 +0100

    fix : delete wrong files

--- src/main/java/sprint1TascaS103_N1_3/CountryQuizMain.java
+++
@@ -1,8 +0,0 @@
-package sprint1TascaS103_N1_3;
-
-public class CountryQuizMain {
-    public static void main(String[] args) {
-        CountryQuiz courntyQuiz = new CountryQuiz();
-        courntyQuiz.startQuiz();
-    }
-}
